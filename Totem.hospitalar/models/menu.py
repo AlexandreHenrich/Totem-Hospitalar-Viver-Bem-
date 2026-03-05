202973819f0978b564dc7f82a87dc4de6753f25c
Aluno <gabriel13.oliveiras@gmail.com>
2026-03-05 20:33:16 -0300

    Foi a alteração aonde estava o self.servico para self;menu, pois necessitava dessa alteração, pois precisava ser o mesmo nome em todos os lugares

--- Totem.hospitalar/models/menu.py
+++ Totem.hospitalar/models/menu.py
@@ -1,32 +1,31 @@
-# aqui é onde fica o menu do totem, ou seja, as opções de atendimento
-
 class Menu:
     def __init__(self):
         self.menu = {
-        1: "atendimento",
-        2: "atendimento preferencial",
-        3: "atendimento 80 anos ou mais",
+            1: "atendimento",
+            2: "atendimento preferencial",
+            3: "atendimento 80 anos ou mais",
+            4: "sair"
         }
 
-def exibir(self):
-        #exibir o menu de serviços
-        print("\n" + "="*30)
-        print("        MENU DE SERVIÇOS        ")
-        print("="*30)
-        for chave, descricao in self.servicos.items():
-            print(f" [{chave}] {descricao}")
-        print("="*30)
+    def exibir(self):
+            #exibir o menu de serviços
+            print("\n" + "="*30)
+            print("        MENU DE SERVIÇOS        ")
+            print("="*30)
+            for chave, descricao in self.menu.items():
+                print(f" [{chave}] {descricao}")
+            print("="*30)
 
-def selecao(self):
-    #selecionar o serviço desejado
-    while True:
-        try:
-            escolha = int(input("Selecione o atendimento desejado: "))
-            if escolha in self.servicos:
-                print(f"\nVocê selecionou: {self.servicos[escolha]}")
-                return escolha
-            else:
-                print("Opção inválida. Por favor, tente novamente.")
-        except ValueError:
-            print("Entrada inválida. Por favor, digite um número.")
+    def selecao(self):
+        #selecionar o serviço desejado
+        while True:
+            try:
+                escolha = int(input("Selecione o atendimento desejado: "))
+                if escolha in self.menu:
+                    print(f"\nVocê selecionou: {self.menu[escolha]}")
+                    return escolha
+                else:
+                    print("Opção inválida. Por favor, tente novamente.")
+            except ValueError:
+                print("Entrada inválida. Por favor, digite um número.")
 
