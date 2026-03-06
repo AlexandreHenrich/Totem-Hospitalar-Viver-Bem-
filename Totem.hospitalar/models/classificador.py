7d8d328ffb1171ca2d2a40099464837037a434e2
Felipe Augusto Rocha <256679597+Felipe-Augusto-Rocha@users.noreply.github.com>
2026-03-05 21:11:19 -0300

    CLASSIFICADOR ATUALIZADO CERTO

--- Totem.hospitalar/models/classificador.py
+++ Totem.hospitalar/models/classificador.py
@@ -2,8 +2,9 @@ class classificador:
     def definir_prioridade(self,paciente):
         if paciente.idade >= 60 or paciente.pcd:
             return "Prioritário"
-        if else paciente.idade <80:
+        elif paciente.idade <80:
             return "Super prioritário"
         else:
             return "Normal"
-        ##atualizado
+        
+        
