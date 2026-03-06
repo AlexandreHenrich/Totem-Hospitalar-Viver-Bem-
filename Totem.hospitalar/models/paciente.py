f311768a39933aac8cdbe9dd45d0dac71f76c41e
Gabriel Oliveira <gabriel13.oliveiras@gmail.com>
2026-03-06 17:04:52 -0300

    Atualizei algo para nao bugar no menu

--- Totem.hospitalar/models/paciente.py
+++ Totem.hospitalar/models/paciente.py
@@ -7,4 +7,7 @@ class Pacientes:
         self.nome = nome
         self.idade = idade
         self.pcd = pcd 
-        
+    
+    def exibir_dados_paciente(self):
+        status_pcd = "Sim" if self.pcd else "Não"
+        return f"Nome: {self.nome} | Idade: {self.idade} | PCD: {status_pcd}"  
